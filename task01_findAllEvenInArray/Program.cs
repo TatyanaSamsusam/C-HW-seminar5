52bce3dfefbe2a1a851b0fb1072617247d9a1373
Tatyana <android87@list.ru>
2022-05-28 00:05:35 +0400

    find all even numbers in array

---
+++ task01_findAllEvenInArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+var newRand = new Random();
+int EvenQty = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = newRand.Next(100, 1000);
+    Console.Write(array[i] + " ");
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    EvenQty = EvenQty + 1;
+}
+Console.WriteLine();
+Console.WriteLine($"в этом массиве {EvenQty} четных чисел(-ла)");
