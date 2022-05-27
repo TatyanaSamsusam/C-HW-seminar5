d38adfd889244ab005bf93ee976691a3facddf14
Tatyana <android87@list.ru>
2022-05-28 00:34:08 +0400

    find difference between min and max in array

---
+++ task03_findDiffBetwennMinMax/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double [] array = new double[5];
+var newRand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(newRand.NextDouble(),1);
+    Console.Write(array[i] + " ");
+}
+
+double min = array[0]; double max = array[0];
+double result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+}
+result = Math.Round(max - min,1);
+
+Console.WriteLine();
+Console.WriteLine($"минимальное число в массиве равно {min}, максимальное число равно {max}. разница между ними равна {result}");
